1e39bcd022d6c8792741f46977af4408642117e4
Isaac <isaachuston823@gmail.com>
2023-06-08 20:44:50 -0300

    Home updt

--- components/Footer.tsx
+++ components/Footer.tsx
@@ -2,8 +2,20 @@ import React from "react";
 
 const Footer: React.FC = () => {
   return (
-    <footer className="py-2 text-center  left-0 bottom-0 w-full">
-      <p className="text-sm text-gray-500">Budget Wise &copy; 2023</p>
+    <footer className="py-2 text-center left-0 bottom-0 w-full">
+      <hr className="border-gray-500 mb-2" />
+      <p className="text-sm text-gray-500 flex justify-center items-center">
+        <a href="/">Home</a>
+        <span className="mx-2">|</span>
+        <a href="/features">Features</a>
+        <span className="mx-2">|</span>
+        <a href="/pricing">Pricing</a>
+        <span className="mx-2">|</span>
+        <a href="/faqs">FAQs</a>
+        <span className="mx-2">|</span>
+        <a href="/about">About</a>
+      </p>
+      <p className="text-sm text-gray-500">© 2023 Company, Inc</p>
     </footer>
   );
 };
