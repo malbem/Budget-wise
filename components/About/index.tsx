40dd5f36761e0ef36c72207dff5d9654e72a055d
Isaac <isaachuston823@gmail.com>
2023-06-09 03:21:59 -0300

    error class

--- components/About/index.tsx
+++ components/About/index.tsx
@@ -23,15 +23,16 @@ const AboutTitle: React.FC = () => {
       </div>
 
       <div className="text-center my-8 pt-[12rem] pb-[12rem]">
-        <div className="inline-block rounded-xl p-[50px] text-white  bg-[#7f7f9f] dark:bg-[#ffffff] shadow-md">
-          <h1 className="text-6xl font-bold mb-4 dark:text-black">Quem Somos</h1>
-          <p className="text-xl px-4 max-w-[750px] pt-[35px] opacity-70 dark:text-black">
+        <div className="inline-block rounded-lg p-4">
+          <h1 className="text-6xl font-bold mb-4">Quem Somos</h1>
+          <p className="text-xl px-4 max-w-[750px] pt-[35px] opacity-70">
             Bem-vindo à Budget Wise! Nós estamos aqui para ajudar você a encontrar o melhor orçamento para o projeto que você precisa.
             Em nosso site, você pode criar uma conta e solicitar orçamentos personalizados. Nós trabalhamos com a melhor rede de fornecedores
             e prestadores de serviços, para que você possa encontrar a melhor oferta para sua empresa. Entre em contato conosco se precisar
             de ajuda em qualquer etapa do processo. Obrigado por escolher nosso site de orçamentos!
           </p>
         </div>
+
       </div>
 
     </header>
