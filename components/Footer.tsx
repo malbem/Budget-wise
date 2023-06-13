2f82dd9c173d0ce31c45491c712d5d5000b3d08d
Isaac <isaachuston823@gmail.com>
2023-06-13 11:27:21 -0300

    Footer 100%

--- components/Footer.tsx
+++ components/Footer.tsx
@@ -1,68 +1,25 @@
 import React from "react";
-// import { useTheme } from "next-themes";
 
 const Footer: React.FC = () => {
-  // const { theme } = useTheme();
-  // const logoSrc = theme === "dark" ? "/images/logo/logo.svg" : "/images/logo/logo-2.svg";
-  return (
-    <footer className="pt-10 text-center">
-      
-      {/* <section className="map1 bg-gray-100 py-10 w-full">
-        <div className="google-map">
-          <iframe
-            title="Google Map"
-            className="w-full h-80"
-            src="https://www.google.com/maps/embed?pb=!1m14!1m12!1m3!1d236.08995400962039!2d-46.40763223567558!3d-18.778567583921824!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!5e0!3m2!1spt-BR!2sbr!4v1681482257247!5m2!1spt-BR!2sbr"
-          ></iframe>
-        </div>
-      </section>
-      <section className="contacts1 py-10">
-        <div className="container mx-auto">
-          <div className="flex items-center justify-center space-x-8">
-            <img src={logoSrc} alt="Budget Wise" className="w-40 h-40" />
-            <div className="card">
-              <div className="card-wrapper flex items-center">
-                <span className="icon-bg icon1"></span>
-                <span className="p-2 mbr-iconfont mobi-mbri-map-pin mobi-mbri"></span>
-                <div className="card-box my-auto">
-                  <h5 className="card-text text-lg">Patos de Minas</h5>
-                  <div>244 Rua de Deus</div>
-                </div>
-              </div>
-            </div>
-            <div className="card">
-              <div className="card-wrapper flex items-center">
-                <span className="icon-bg icon2"></span>
-                <span className="p-2 mbr-iconfont mobi-mbri-letter mobi-mbri"></span>
-                <div className="card-box my-auto">
-                  <h5 className="card-text text-lg">support@budgetwise.com</h5>
-                  <div>connect@budgetwise.com</div>
-                </div>
-              </div>
-            </div>
-            <div className="card">
-              <div className="card-wrapper flex items-center">
-                <span className="icon-bg icon3"></span>
-                <span className="p-2 mbr-iconfont mobi-mbri-phone mobi-mbri"></span>
-                <div className="card-box my-auto">
-                  <h5 className="card-text text-lg">0 (800) 123 45 67</h5>
-                  <div>0 (800) 231 54 76</div>
-                </div>
-              </div>
-            </div>
-          </div>
-        </div>
-      </section>
 
-      <section className="footer1 bg-dark py-4">
-        <div className="container mx-auto">
-          <div className="flex justify-center items-center">
-            <p className="text-white text-lg">
-              © 2023 Budget Wise - All Rights Reserved
-            </p>
+  return (
+    <footer>
+      <div className=" pt-80 container px-6 py-8 mx-auto">
+
+        <hr className="my-10 border-dark dark:border-white" />
+
+        <div className="flex flex-col items-center sm:flex-row sm:justify-between">
+          <p className="text-sm text-dark dark:text-white ">© Copyright 2023. All Rights Reserved.</p>
+
+          <div className=" dark:text-white flex mt-3 -mx-2 sm:mt-0">
+            <a href="#" className="mx-2 text-sm hover:opacity-60 text-dark dark:text-white transition-colors duration-300 "> Teams </a>
+
+            <a href="#" className="mx-2 text-sm hover:opacity-60 text-dark dark:text-white transition-colors duration-300 "> Privacy </a>
+
+            <a href="#" className="mx-2 text-sm hover:opacity-60 text-dark  dark:text-white transition-colors duration-300"> Cookies </a>
           </div>
         </div>
-      </section> */}
+      </div>
     </footer>
   );
 };
