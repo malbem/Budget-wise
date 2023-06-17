96343fe0eaf694f7d30c0169376c6604a73b9fa1
Gabriel Rodrigues <gabrielrb@unipam.edu.br>
2023-06-17 12:41:00 -0300

    concerto erro strol to top

--- components/ScrollToTop/index.tsx
+++ components/ScrollToTop/index.tsx
@@ -18,7 +18,7 @@ export default function ScrollToTop() {
         setIsVisible(false);
       }
     };
-
+ 
     window.addEventListener("scroll", toggleVisibility);
 
     return () => window.removeEventListener("scroll", toggleVisibility);
