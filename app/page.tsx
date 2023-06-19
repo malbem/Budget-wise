9ad2a17adf1533953ae1927cf1e7f76903c8097e
victornascimentodiniz <130411984+victornascimentodiniz@users.noreply.github.com>
2023-06-19 14:09:34 -0300

    Update page.tsx

--- app/page.tsx
+++ app/page.tsx
@@ -4,7 +4,7 @@ import SvgComponent from "@/components/SvgComponent";
 import Footer from "@/components/Footer";
 import AboutTitle from "@/components/About"; 
  
-
+ 
 export default function Home() {
   return (
     <>
