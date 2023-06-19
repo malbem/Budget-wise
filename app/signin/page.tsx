5e9fbe4395fb489df4dfd9d07161e5540e4525db
Victor Matos <130411936+victormatosx@users.noreply.github.com>
2023-06-19 17:23:21 -0300

    Update page.tsx

--- app/signin/page.tsx
+++ app/signin/page.tsx
@@ -11,7 +11,7 @@ const LoginModal: React.FC = () => {
     <section className="relative z-10 overflow-hidden pt-36 pb-16 md:pb-20 lg:pt-[180px] lg:pb-28"> 
       <div className="container"> 
         <div className="-mx-4 flex flex-wrap">
-          <div className="w-full px-4">
+          <div className="w-full px-4">  
             <div className="mx-auto max-w-[500px] rounded-md bg-primary bg-opacity-5 py-10 px-6 dark:bg-dark sm:p-[60px]">
               <h3 className="mb-3 text-center text-2xl font-bold text-black dark:text-white sm:text-3xl">
                 Faça login em sua conta
