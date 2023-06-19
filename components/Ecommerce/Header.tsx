91e38ffea0f35345d3cf47ec8887c405f40a57d6
victornascimentodiniz <130411984+victornascimentodiniz@users.noreply.github.com>
2023-06-19 14:04:35 -0300

    Update Header.tsx

--- components/Ecommerce/Header.tsx
+++ components/Ecommerce/Header.tsx
@@ -1,6 +1,6 @@
 "use client"
 import React, { useState, useEffect, useRef } from "react";
-import Image from "next/image";
+import Image from "next/image"; 
 import Link from "next/link";
 import ThemeToggler from "../ThemeToggler"; 
 import { FaSearch, FaShoppingCart, FaHeart, FaUser, FaBars } from "react-icons/fa";
