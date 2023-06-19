72038ac092f04133793b7677f19a801dc56b7853
victornascimentodiniz <130411984+victornascimentodiniz@users.noreply.github.com>
2023-06-19 14:35:50 -0300

    Update team.tsx

--- components/About/team.tsx
+++ components/About/team.tsx
@@ -26,7 +26,7 @@ const Team: React.FC = () => {
             const j = Math.floor(Math.random() * (i + 1));
             [shuffled[i], shuffled[j]] = [shuffled[j], shuffled[i]];
         }
-        return shuffled;
+        return shuffled; 
     };
 
     return (
