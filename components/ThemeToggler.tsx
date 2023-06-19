e318f9f608f35f9dae2903b0abd33868d205efde
Victor Matos <130411936+victormatosx@users.noreply.github.com>
2023-06-19 17:17:14 -0300

    Update ThemeToggler.tsx

--- components/ThemeToggler.tsx
+++ components/ThemeToggler.tsx
@@ -3,7 +3,7 @@ import { useTheme } from "next-themes";
 const ThemeToggler = ({onClick}) => {
   const { theme, setTheme } = useTheme();
   return ( 
-    <button
+    <button 
       onClick={ () =>  setTheme(theme === "dark" ? "light" : "dark")}
       className="bg-gray-2 dark:bg-dark-bg flex h-9 w-9 cursor-pointer items-center justify-center rounded-full text-black dark:text-white md:h-14 md:w-14"
     >
