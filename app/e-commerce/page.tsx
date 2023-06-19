60b25dff036311f102bba187178a8932bb46f587
Victor Matos <130411936+victormatosx@users.noreply.github.com>
2023-06-19 17:22:23 -0300

    Update page.tsx

--- app/e-commerce/page.tsx
+++ app/e-commerce/page.tsx
@@ -9,8 +9,8 @@ const Ecommerce: React.FC = () => {
     return <>   
         <Header /> 
         <section className="relative z-10 overflow-hidden pt-36 pb-16 md:pb-20 lg:pt-[180px] lg:pb-28">
-            <HomeEcommerce/>
-        </section> 
+            <HomeEcommerce/> 
+        </section>  
         <Footer />
 
     </>;
