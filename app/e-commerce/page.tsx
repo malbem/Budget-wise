e583fa1bf7ac15a9c7e7747feb4106b14702bd2a
Gabrew <130412006+Gabrew@users.noreply.github.com>
2023-06-19 10:46:30 -0300

    Update page.tsx

--- app/e-commerce/page.tsx
+++ app/e-commerce/page.tsx
@@ -6,7 +6,7 @@ import Footer from '@/components/Footer';
 import React from 'react';
    
 const Ecommerce: React.FC = () => { 
-    return <> 
+    return <>  
         <Header />
         <section className="relative z-10 overflow-hidden pt-36 pb-16 md:pb-20 lg:pt-[180px] lg:pb-28">
 
