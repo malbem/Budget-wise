6ec152e3ff973cd9f245b1d02b3106aa9f0c2ea0
Victor Matos <130411936+victormatosx@users.noreply.github.com>
2023-06-19 17:19:07 -0300

    Update page.tsx

--- app/Templete/page.tsx
+++ app/Templete/page.tsx
@@ -5,7 +5,7 @@ import React from 'react';
 {/* Altere o nome da page */}
 const Templete: React.FC = () => {
     return <>  
-         
+          
         <section className="relative z-10 overflow-hidden pt-36 pb-16 md:pb-20 lg:pt-[180px] lg:pb-28">
             Sua page pode ficar aqui, no entando arrume todos componentes abaixo do section!
         </section>
