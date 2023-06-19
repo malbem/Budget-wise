92107b4e37da3d74456a3c4a1a78ca8a0078ecd0
Gabrew <130412006+Gabrew@users.noreply.github.com>
2023-06-19 10:49:02 -0300

    Update page.tsx

--- app/page.tsx
+++ app/page.tsx
@@ -2,7 +2,7 @@
 
 import SvgComponent from "@/components/SvgComponent";
 import Footer from "@/components/Footer";
-import AboutTitle from "@/components/About";
+import AboutTitle from "@/components/About"; 
  
 
 export default function Home() {
