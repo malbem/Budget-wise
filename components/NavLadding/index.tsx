a560e3c4daf69b9d5ad357df4ebe938225cfc433
victornascimentodiniz <130411984+victornascimentodiniz@users.noreply.github.com>
2023-06-19 14:06:36 -0300

    Update index.tsx

--- components/NavLadding/index.tsx
+++ components/NavLadding/index.tsx
@@ -1,6 +1,6 @@
 "use client"
 import Image from "next/image";
-import Link from "next/link";
+import Link from "next/link"; 
 import { useEffect, useState } from "react";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import { faBars } from "@fortawesome/free-solid-svg-icons";
