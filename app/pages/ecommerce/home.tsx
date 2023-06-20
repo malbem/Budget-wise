a61ac612ab8eb14aa3380f20ebbed65507bc5831
Victor Matos <130411936+victormatosx@users.noreply.github.com>
2023-06-20 19:22:29 -0300

    Update home.tsx

--- app/pages/ecommerce/home.tsx
+++ app/pages/ecommerce/home.tsx
@@ -2,14 +2,14 @@ import React from "react";
 import Footer from "@/components/Footer";
 import HomeEcommerce from "@/components/Ecommerce";
 import Header from "@/components/Ecommerce/Header";
-
+import Carousel from "@/components/Ecommerce/carousel"
 
 const ecommerce: React.FC = () => {
 
     return <>
 
         <Header />
-        <
+        <Carousel/>
         <Footer />
 
     </>
