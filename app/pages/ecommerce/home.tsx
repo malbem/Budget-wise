6f99d8901f2f3809923a73f2f71c7ef0d2b5a880
Victor Matos <130411936+victormatosx@users.noreply.github.com>
2023-06-20 19:19:57 -0300

    Update home.tsx

--- app/pages/ecommerce/home.tsx
+++ app/pages/ecommerce/home.tsx
@@ -1,1 +1,20 @@
+import React from "react";
+import Footer from "@/components/Footer";
+import HomeEcommerce from "@/components/Ecommerce";
+import Header from "@/components/Ecommerce/Header";
 
+
+const ecommerce: React.FC = () => {
+
+    return <>
+
+        <Header />
+        <
+        <Footer />
+
+    </>
+
+}
+
+
+export default ecommerce
