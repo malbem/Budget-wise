96c98d8209c0f58240b0467665d65859fa401676
Victor Matos <130411936+victormatosx@users.noreply.github.com>
2023-06-20 20:20:47 -0300

    Update Header.tsx

--- components/Ecommerce/Header.tsx
+++ components/Ecommerce/Header.tsx
@@ -50,7 +50,7 @@ const Header = () => {
     };
 
     const menuItems = [
-        { label: "Home", url: "/pages/ecommerce/home", onClick: handleModalClose },
+        { label: "Home", url: "/pages/home", onClick: handleModalClose },
         {
             label: "Serviços",
             url: "#",
